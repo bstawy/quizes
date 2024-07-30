12bf5ce883c1ddf8bfc203786570405eadabcb9a
bastawy <125200508@sha.edu.eg>
2024-07-30 05:07:20 +0300

    Create LayoutScreen with presistent bottom nav bar

---
+++ lib/layout/widgets/bottom_nav_bar_tab.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+
+import '../../core/config/texts/text_styles.dart';
+import '../../core/config/theme/colors_manager.dart';
+
+PersistentTabConfig bottomNavBarTab({
+  required Widget screen,
+  required String title,
+  required String iconPath,
+  required String inactiveIconPath,
+}) {
+  return PersistentTabConfig(
+    screen: screen,
+    item: ItemConfig(
+      title: "\t\t$title",
+      textStyle: TextStyles.font16WhiteBold,
+      icon: SvgPicture.asset(
+        iconPath,
+        width: 26.w,
+        height: 26.h,
+      ),
+      inactiveIcon: SvgPicture.asset(
+        inactiveIconPath,
+        width: 26.w,
+        height: 26.h,
+      ),
+      activeForegroundColor: Colors.white,
+      activeColorSecondary: ColorsManager.teal,
+    ),
+  );
+}
