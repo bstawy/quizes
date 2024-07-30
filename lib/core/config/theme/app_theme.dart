6e21fc9c32ce4d04fe8390c9282b416534acaad3
bastawy <125200508@sha.edu.eg>
2024-07-30 05:56:26 +0300

    Configure scaffold background color and primary color

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -5,8 +5,14 @@ import 'colors_manager.dart';
 class AppTheme {
   static final ThemeData lightThemeData = ThemeData(
     useMaterial3: true,
+    scaffoldBackgroundColor: ColorsManager.lightGrey,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: ColorsManager.lightGrey,
+      centerTitle: false,
+    ),
     colorScheme: ColorScheme.fromSeed(
       seedColor: ColorsManager.teal,
+      primary: ColorsManager.teal,
       surface: ColorsManager.lightGrey,
     ),
     fontFamily: 'Montserrat',
