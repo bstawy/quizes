ae2f7f6b4db02d81ed54c864bc3d96d1e1b2274e
bastawy <125200508@sha.edu.eg>
2024-07-30 13:32:01 +0300

    Create widget to show unfinished quiz

---
+++ lib/pages/quizes/widgets/unfinished_quiz_item_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+
+import 'quiz_info_widget.dart';
+import 'quiz_title_and_description.dart';
+import 'view_quiz_button.dart';
+
+class UnfinishedQuizItemWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  final Duration duration;
+  final int questionsNumber;
+
+  const UnfinishedQuizItemWidget({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.duration,
+    required this.questionsNumber,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        QuizTitleAndDescription(title: title, description: description),
+        Gap(24.h),
+        Row(
+          children: [
+            QuizInfoWidget(
+              title: "الأسئلة",
+              info: questionsNumber.toString(),
+              imagePath: "assets/images/questions_image.png",
+            ),
+            Gap(18.w),
+            QuizInfoWidget(
+              title: "مدة الإختبار",
+              info: "${duration.inMinutes} دقيقة",
+              imagePath: "assets/images/unfinished_duration_image.png",
+            ),
+            const Spacer(),
+            const ViewQuizButton(),
+          ],
+        ),
+      ],
+    );
+  }
+}
